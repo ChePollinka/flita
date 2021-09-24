31b8d8213fa602b286f528324b500608c3b2a7c6
ChePollinka <pollina.151@gmail.com>
2021-09-24 03:24:47 +0300

    first commit

---
+++ first_hw.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdint.h"
+
+int main() {
+
+    uint64_t dec = 0;
+    char buf = getchar();
+    int k = 0;
+
+    while ( buf != '\n' )
+    {
+        if ( (buf == '0') || (buf == '1') )
+        {
+            if ( dec >= ((uint64_t) 1) << 63 )
+            {
+                printf ("Overflow error\n");
+            }
+            dec = dec << 1;
+            dec += buf - '0';
+            
+        }
+        else 
+        { 
+            printf ("Number error\n"); 
+            k = 1;
+        }
+
+        buf = getchar();
+    }
+    if (k == 0) { printf ("decision: [%lu]\n", dec);}
+
+    return 0;
+}
